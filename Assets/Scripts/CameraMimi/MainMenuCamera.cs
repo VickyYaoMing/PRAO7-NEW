69cf294a1926efab7a77dde381ae602021913255
DataConstrictor <noel.kva@gmail.com>
2025-11-19 14:48:05 +0100

    Added back a script

---
+++ Assets/Scripts/CameraMimi/MainMenuCamera.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    [SerializeField]Camera camera;
+    Vector3 startPosition;
+    [SerializeField]GameObject emptyEndPos;
+    Vector3 endPos;
+
+    // apparently I can get camera as a inherited object but idk how to do that so it is serialized lol
+    // we need the empty end pos because that is the position that is going to be lerped too, makes it so that if we do scale the elevator, it will not completely screw over everything.
+
+    void Start()
+    {
+        if (camera != null)
+        {
+            startPosition = camera.GetComponent<Transform>().position;
+            endPos = emptyEndPos.transform.position;
+            camera.fieldOfView = 60;
+        }
+    }
+
+    void Update()
+    {
+        startPosition = camera.GetComponent<Transform>().position;
+        camera.GetComponent<Transform>().position = Vector3.Lerp(startPosition, endPos, 0.01f);
+
+    }
+}
