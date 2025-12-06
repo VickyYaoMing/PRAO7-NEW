c9d04411d891d0f55faff476930304c49c4c7e1e
Lioxsaf <Lizandro.cruz01@gmail.com>
2025-12-06 19:14:00 +0100

    endscene done?

--- Assets/Scripts/UI/WhiteBoard/WhiteboardUi.cs
+++ Assets/Scripts/UI/WhiteBoard/WhiteboardUi.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+
 
 public class WhiteboardUi : MonoBehaviour
 {
 
-    [Header("Scene Stuff")]
+    [Header("Score Display")]
+    [SerializeField] public TMP_Text scoreText;
+
+    [Header("Scene Change")]
     private Dictionary<string, string> nextSceneMap = new Dictionary<string, string>
     {
         {StringLiterals.DAY1_SCENE, StringLiterals.DAY2_SCENE },
@@ -15,9 +18,78 @@ public class WhiteboardUi : MonoBehaviour
         {StringLiterals.DAY3_SCENE, StringLiterals.STARTMENU_SCENE },
     };
 
+    [Header("Table Cells")]
+    [Header("Row 1")]
+    [SerializeField] public TMP_Text row1Col1;
+    [SerializeField] public TMP_Text row1Col2;
+    [SerializeField] public TMP_Text row1Col3;
+    [SerializeField] public TMP_Text row1Col4;
+
+    [Header("Row 2")]
+    [SerializeField] public TMP_Text row2Col1;
+    [SerializeField] public TMP_Text row2Col2;
+    [SerializeField] public TMP_Text row2Col3;
+    [SerializeField] public TMP_Text row2Col4;
+
+    [Header("Row 3")]
+    [SerializeField] public TMP_Text row3Col1;
+    [SerializeField] public TMP_Text row3Col2;
+    [SerializeField] public TMP_Text row3Col3;
+    [SerializeField] public TMP_Text row3Col4;
+
+    [Header("Row 4")]
+    [SerializeField] public TMP_Text row4Col1;
+    [SerializeField] public TMP_Text row4Col2;
+    [SerializeField] public TMP_Text row4Col3;
+    [SerializeField] public TMP_Text row4Col4;
+
+
+
     private void Start()
     {
-        
+        UpdateScore(42);
+
+
+        // temp placeholder data, should be called like this elsewhere? idk how else to do this lol 
+        string[] placeholderNames = { "Printing Task", "Coffee Task", "Cleanup Task", "Dartboard Task" };
+        float[] placeholderTime = { 10.4f, 48.23f, 130.32f, 44.23f };
+        int[] placeholderClicks = { 10, 45, 43, 23 };
+        int[] placeholderTimesPlayer = { 2309, 12319, 2393, 22 };
+
+
+        UpdateTable(placeholderNames, placeholderTime, placeholderClicks, placeholderTimesPlayer);
+
+    }
+
+    public void UpdateScore(int score)
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+
+    public void UpdateTable(string[] names, float[] avgTime, int[] avgClicks, int[] timesPlayed)
+    {
+        int rowCount = Mathf.Min(4, names.Length);
+
+        row1Col1.text = names[0];
+        row1Col2.text = avgTime[0].ToString();
+        row1Col3.text = avgClicks[0].ToString();
+        row1Col4.text = timesPlayed[0].ToString();
+
+        row2Col1.text = names[1];
+        row2Col2.text = avgTime[1].ToString();
+        row2Col3.text = avgClicks[1].ToString();
+        row2Col4.text = timesPlayed[1].ToString();
+
+        row3Col1.text = names[2];
+        row3Col2.text = avgTime[2].ToString();
+        row3Col3.text = avgClicks[2].ToString();
+        row3Col4.text = timesPlayed[2].ToString();
+
+        row4Col1.text = names[3];
+        row4Col2.text = avgTime[3].ToString();
+        row4Col3.text = avgClicks[3].ToString();
+        row4Col4.text = timesPlayed[3].ToString();
     }
 
     // Change Scene, this is written with the assumption that end game screen does not become main scene
