cdbf7f606372b11ac0386d6eddbc335984a4440f
Karincjansson <Karin.c.jansson@outlook.com>
2026-01-03 15:34:19 +0100

    3/1

--- Assets/Scripts/Helpers/GameData.cs
+++ Assets/Scripts/Helpers/GameData.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public static class GameData
 {
-    public static readonly float TaskTimer = 20f;
-    public static readonly float DayTimer = 80f;
+    public static readonly float TaskTimer = 40f;
+    public static readonly float DayTimer = 100f;
     public static readonly int maxTaskScore = 100;
     public static readonly int yellowTaskScore = 50;
     public static readonly int redTaskScore = 25;
